6702535d7b7ea5bc561fd0c59482097140b561a9
felix <gogoehd@shinsegae.com>
2024-06-07 13:38:24 +0900

    #4 Prop Types - App 분리

---
+++ src/section-apps/AppSection4.js
@@ -0,0 +1,26 @@
+import './App.css';
+import {Btn} from "./components/section4/btn";
+import {useState} from "react";
+import React from "react";
+import PropTypes from "prop-types";
+
+const MemorizedBtn = React.memo(Btn);
+Btn.propTypes = {
+    text: PropTypes.string.isRequired,
+    fontSize: PropTypes.number,
+    background: PropTypes.string.isRequired,
+    changeValue: PropTypes.func
+}
+
+function AppSection4() {
+    const [value, setValue] = useState("Save Changes");
+    const changeValue = () => setValue("Revert Changes");
+    return (
+        <div className="App">
+            <MemorizedBtn background={"tomato"} text={value} changeValue={changeValue}></MemorizedBtn>
+            <MemorizedBtn fontSize={18} background={"skyblue"} text="Continue"></MemorizedBtn>
+        </div>
+    );
+}
+
+export default AppSection4;
