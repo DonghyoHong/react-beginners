f5641cf7996f8ca877e2e52b6090d417f17f8b06
felix <gogoehd@shinsegae.com>
2024-06-07 13:20:01 +0900

    #4.1 memo

--- src/App.js
+++ src/App.js
@@ -1,17 +1,17 @@
 import './App.css';
 import {Btn} from "./components/section4/btn";
+import {useState} from "react";
+import React from "react";
+
+const MemorizedBtn = React.memo(Btn);
 
 function App() {
+    const [value, setValue] = useState("Save Changes");
+    const changeValue = () => setValue("Revert Changes");
     return (
         <div className="App">
-            <Btn data={{
-                text: "Save Btn"
-                , background: "tomato"
-            }}></Btn>
-            <Btn data={{
-                text: "Confirm Btn"
-                , background: "skyblue"
-            }}></Btn>
+            <MemorizedBtn background={"tomato"} text={value} changeValue={changeValue}></MemorizedBtn>
+            <MemorizedBtn background={"skyblue"} text="Continue"></MemorizedBtn>
         </div>
     );
 }
--- src/components/section4/btn.js
+++ src/components/section4/btn.js
@@ -1,19 +1,25 @@
-export function Btn({data}) {
+import React from "react";
+
+export function Btn({text, background, changeValue}) {
     //props 대신 {data} 로 컴포넌트에 넘겨주기로 한 데이터 형태를 받을 수 있음
     // -> data.text, data.backgroun
     // props를 쓰게 되면 컴포넌트에 선언되어 있는 것들을 가져올 수 잇음
     // -> props.data.text, props.data.backgroun
-    console.log(data);
+    console.log(`${text} was rendered.`);
+    // Re-Render가 다시 되지 않도록 Memo를 이용하면 됨
+
 
     return (
         <div>
-            <button style={{
-                background: data.background
-                , color: "white"
-                , padding: "10px 20px"
-                , border: 0
-                , borderRadius: 10
-            }}>{data.text}
+            <button onClick={changeValue}
+                    style={{
+                        background: background
+                        , color: "white"
+                        , padding: "10px 20px"
+                        , border: 0
+                        , borderRadius: 10
+                        , fontSize: 16
+                    }}>{text}
             </button>
         </div>
     );
