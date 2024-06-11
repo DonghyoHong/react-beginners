b0834cef600ef10b522e9eab20847736afb36bfc
DonghyoHong <gogoehdgy@naver.com>
2024-06-11 11:18:40 +0900

    #7 basename 추가

--- src/index.js
+++ src/index.js
@@ -1,13 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
-import {HashRouter} from "react-router-dom";
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-    <HashRouter basename={process.env.PUBLIC_URL}>
-        <App/>
-    </HashRouter>
+    <App/>
     /*<React.StrictMode>
         <App/>
     </React.StrictMode>*/
